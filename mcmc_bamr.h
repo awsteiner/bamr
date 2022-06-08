f615db17ebb8abe393649d43ea2b5d60264a5b2c
Andrew W. Steiner <awsteiner@mykolab.com>
2022-06-08 15:18:10 -0400

    Adding emulator class.

--- mcmc_bamr.h
+++ mcmc_bamr.h
@@ -38,6 +38,7 @@
 
 #include <o2scl/hdf_file.h>
 #include <o2scl/mcmc_para_new.h>
+#include <o2scl/emulator.h>
 
 #ifdef BAMR_READLINE
 #include <o2scl/cli_readline.h>
@@ -62,7 +63,91 @@ namespace bamr {
   
   typedef std::function<int(const ubvector &,double,
 			    std::vector<double> &,model_data &)> fill_funct;
+  
+  /** \brief A specialized emulator for this code
+      
+      Note that this emulator has a data type of array<double,ndat>,
+      because that is what the MCMC is using, but internally em1 uses a
+      data type of vector<double>, since not all output fields are
+      always emulated.
+  */
+  class emulator_rs : public o2scl::emulator_unc<model_data,
+                                                 model_data,
+                                                 ubvector> {
+    
+  public:
+    
+    int mpi_rank;
+    
+    int mpi_size;
+    
+    /** \brief The internal generic emulator
+     */
+    o2scl::emulator_interpm_idw_table<std::vector<double>,ubvector> em1;
+    
+    /** \brief The second internal generic emulator
+     */
+    o2scl::emulator_interpm_krige_table<std::vector<double>,ubvector> em2;
+    
+    /* \brief Pointer to the data_eval object for function evaluations
+     */
+    bamr_class *bcp;
 
+    std::ofstream *sop;
+    
+    /** \brief List of parameters and output quantities (including log_wgt)
+     */
+    std::vector<std::string> list;
+    
+    /** \brief Tables from which to train
+     */
+    o2scl::table_units<> table;
+    
+    /** \brief List of filenames containing training tables
+     */
+    std::vector<std::string> files;
+    
+    /** \brief Number of parameters
+     */
+    size_t np;
+    
+    /** \brief Number of output quantities for em1
+     */
+    size_t nout;
+    
+    /** \brief Mapping of parameters to indices
+     */
+    o2scl::vec_index pvi;
+    
+    /** \brief Mapping of output data to array indices
+     */
+    o2scl::vec_index dvi;
+    
+    /** \brief Random number generator
+     */
+    o2scl::rng<> r;
+    
+    emulator_rs();
+    
+    /** \brief Train the emulator with the data in \c tab_train
+     */
+    void train(o2scl::table_units<> &tab_train, o2scl::vec_index &pvii,
+               o2scl::vec_index &dvii, bamr_class *bcpi,
+               std::ofstream *sopi);
+    
+    /** \brief Evaluate the emulator, and or the full function if necessary
+     */
+    virtual int eval(size_t n, const ubvector &p, double &log_wgt,
+                     model_data &dat);
+    
+    /** \brief Evaluate the emulator, and or the full function if necessary
+     */
+    virtual int eval_unc(size_t n, const ubvector &p, double &log_wgt,
+                         double &lw_unc, model_data &dat,
+                         model_data &dat_unc);
+    
+  };
+  
   /** \brief Statistical analysis of EOS from M and R constraints
 
       \comment
