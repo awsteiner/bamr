c617a787957e82c6fa59873ad2d5dd1fb6728b51
M. H. Anik <anik@vols.utk.edu>
2023-01-20 11:50:31 -0500

    Fixed the size of ret_value_counts vector

--- mcmc_bamr.cpp
+++ mcmc_bamr.cpp
@@ -681,7 +681,7 @@ int mcmc_bamr::mcmc_init() {
   // default return values in models.h
   this->ret_value_counts.resize(this->n_threads);
   for(size_t it=0;it<this->n_threads;it++) {
-    this->ret_value_counts[it].resize(24);
+    this->ret_value_counts[it].resize(25);
   }
 
   // Copy parameter values to all of the model objects
