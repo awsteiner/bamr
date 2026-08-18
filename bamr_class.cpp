50fd73a8b9587bd75b9569b317e4ca27ed86d606
Anik Hasan <anikmh1@outlook.com>
2026-08-18 11:01:23 -0400

    Backed up old bamr_class

--- bamr_class.cpp
+++ bamr_class.cpp
@@ -1,3 +1,27 @@
+/*
+  -------------------------------------------------------------------
+  
+  Copyright (C) 2012-2026, Mohammad Al-Mamun, Mahmudul Hasan Anik, 
+  and Andrew W. Steiner
+  
+  This file is part of Bamr.
+  
+  Bamr is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  
+  Bamr is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with Bamr. If not, see <http://www.gnu.org/licenses/>.
+
+  -------------------------------------------------------------------
+*/
+
 #include "bamr_class.h"
 
 #include <o2scl/vector.h>
